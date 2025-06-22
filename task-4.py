598cd9a79051a6115a29838d002524657acfd8e0
serhiisydorenko-wdl <serhiisydorenko91@gmail.com>
2025-06-22 09:51:49 +0200

    task 4 is done

---
+++ task-4.py
@@ -0,0 +1,62 @@
+import uuid
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+class Node:
+    def __init__(self, key, color="lightgreen"):
+        self.left = None
+        self.right = None
+        self.val = key
+        self.color = color
+        self.id = str(uuid.uuid4())
+
+
+def build_heap_tree(heap, index=0):
+    """Рекурсивно будує дерево з масиву купи"""
+    if index >= len(heap):
+        return None
+
+    node = Node(heap[index])
+    left_index = 2 * index + 1
+    right_index = 2 * index + 2
+
+    node.left = build_heap_tree(heap, left_index)
+    node.right = build_heap_tree(heap, right_index)
+
+    return node
+
+
+def add_edges(graph, node, pos, x=0, y=0, layer=1):
+    if node is not None:
+        graph.add_node(node.id, color=node.color, label=node.val)
+        if node.left:
+            graph.add_edge(node.id, node.left.id)
+            l = x - 1 / 2 ** layer
+            pos[node.left.id] = (l, y - 1)
+            add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
+        if node.right:
+            graph.add_edge(node.id, node.right.id)
+            r = x + 1 / 2 ** layer
+            pos[node.right.id] = (r, y - 1)
+            add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
+    return graph
+
+
+def draw_tree(tree_root):
+    tree = nx.DiGraph()
+    pos = {tree_root.id: (0, 0)}
+    tree = add_edges(tree, tree_root, pos)
+
+    colors = [node[1]['color'] for node in tree.nodes(data=True)]
+    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}
+
+    plt.figure(figsize=(8, 5))
+    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2000, node_color=colors)
+    plt.title("Бінарна купа як дерево")
+    plt.show()
+
+if __name__ == "__main__":
+  heap_array = [1, 3, 5, 7, 9, 6, 8]  # Це мін-купа
+  root = build_heap_tree(heap_array)
+  draw_tree(root)
